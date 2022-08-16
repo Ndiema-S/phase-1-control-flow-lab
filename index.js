90571730645c6d898f15a5b9984fda6bb4a75837
Sheilah <sheilahndiema@gmail.com>
2022-08-16 23:57:32 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+
+  if (ride <= 400)
+
+    return "This one is on me!";
+
+  else if (ride > 2500)
+
+    return "No can do.";
+
+  else if (ride > 2000)
+
+    return "I will gladly take your thirty bucks.";
+
+}
+
+function ternaryCheckCity(city) {
+
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+
+    case "generous":
+      return "Thank you so much."
+
+    case "not as generous":
+      return "Thank you."
+
+    default:
+      return "Bye."
+
+
+  }
 }
